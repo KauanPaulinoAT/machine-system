9a3b4af16ae22222f082468f566ad6a1105a06de
manoela <manoela.santos@atgroup.com.br>
2025-07-29 11:37:45 -0300

    :sparkles: feat: adicionando validaçao de erros da controller

--- machine-backend/src/controller/machineController.ts
+++ machine-backend/src/controller/machineController.ts
@@ -1,43 +1,63 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as machineService from "../services/machineService";
 
-export const getMaquina = async (req: Request, res: Response) => {
+export const getMaquina = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const machines = await machineService.getMaquina();
-    res.status(200).json(machines);
+
+   res.status(200).json({
+      statusCode: 200,
+      message: "Listagem concluida com sucesso",
+     
+    });
   } catch (error) {
-    console.error("Erro ao realizar listagem ", error)
+    next(error);
   }
 };
 
-export const newMaquina = async (req: Request, res: Response) => {
+export const newMaquina = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, tipo } = req.body;
     const machine = await machineService.newMaquina({ name, tipo });
-    res.status(201).json(machine);
-  } catch (error) {
-    console.log("Erro ao adicionar maquina", error)
-  }
 
+    res.status(204).json({
+      statusCode: 204,
+      message: "Adicionado com sucesso",
+      machine
+    });
+  } catch (error) {
+    next(error)
+  }
 };
 
-export const updateMaquina = async (req: Request, res: Response) => {
+export const updateMaquina = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
     const { name, tipo } = req.body;
+
     const machine = await machineService.updateMaquina(id, { name, tipo });
-    res.json(machine);
+
+    res.status(204).json({
+      statusCode: 204,
+      message: "Atualizado com sucesso",
+      machine
+    });
   } catch (error) {
-    console.log("Erro ao atualizar maquina", error)
+    next(error);
   }
 };
 
-export const deleteMaquina = async (req: Request, res: Response) => {
+export const deleteMaquina = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { id } = req.params;
+
     await machineService.deleteMaquina(id);
-    res.status(204).send();
+
+    res.status(204).json({
+      statusCode: 204,
+      message: "Maquina removida com sucesso"
+    });
   } catch (error) {
-    console.log("Erro ao excluir maquina", error)
+    next(error);
   }
-};
+}; 
